4a977719ad5a69db91cfd13837d893c7c4f136fa
osaddouq <saddouqoussama@gmail.com>
2026-06-29 14:03:18 +0100

    declarationsJS1

--- declarations.js
+++ declarations.js
@@ -1,16 +1,26 @@
 const escapeStr =  "`\\/ \" '  ";
 const arr = [4 , '2'];
+Object.freeze(arr);
+
 const obj = {
     str : "oussama" , 
     num : 2026 ,
     bool : false ,
     undef : undefined,
 }
+Object.freeze(obj);
 
 const nested = {
     arr = [4 , undefined, "2"],
-    obj
+    obj = {
+        str : "oussama" , 
+        num : 2026 ,
+        bool : false ,
+},
 };
-Object.freeze(arr);
-Object.freeze(obj);
+
 Object.freeze(nested);
+Object.freeze(nested.arr);
+Object.freeze(nested.obj);
+
+
