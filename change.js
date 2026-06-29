e331c06a5a84e6d33b72171a0722a25b12be1f70
osaddouq <saddouqoussama@gmail.com>
2026-06-29 17:53:47 +0100

    change

--- change.js
+++ change.js
@@ -1,8 +1,8 @@
 
 function get(key){
-    return sourceObject[key]
+    return sourceObject[key];
 }
 function set (key,value){
     sourceObject[key] = value;
-    return sourceObject[key]
+    return sourceObject[key];
 }
