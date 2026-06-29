84466ee7d3e584d3b2a722ab4b0ef76f6949c184
osaddouq <saddouqoussama@gmail.com>
2026-06-29 14:29:33 +0100

    declarationsJS

--- declarations.js
+++ declarations.js
@@ -1,5 +1,5 @@
-const escapeStr = " ` \\ / \" ' ";
-const arr = [4, '2'];
+const escapeStr = "`\\/'\"";
+const arr = [4, "2"];
 Object.freeze(arr);
 
 const obj = {
@@ -11,15 +11,15 @@ const obj = {
 Object.freeze(obj);
 
 const nested = {
-    arr =[4, undefined, "2"],
-    obj = {
+    arr :[4, undefined, "2"],
+    obj : {
         str: "oussama",
         num: 2026,
         bool: false,
     },
 };
 
-Object.freeze(nested)
+
 Object.freeze(nested.arr)
 Object.freeze(nested.obj)
-
+Object.freeze(nested)
