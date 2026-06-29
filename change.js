8ca3603949a16a9a784b503b189669a9f1b2de68
osaddouq <saddouqoussama@gmail.com>
2026-06-29 18:03:42 +0100

    change1

--- change.js
+++ change.js
@@ -1,33 +1,7 @@
-
-const sourceObject = {
-  num: 42,
-  bool: true,
-  str: "some text",
-  log: console.log,
-};
-
-
-
-function get(key){
+function get(key) {
     return sourceObject[key]
 }
-function set (key,value){
+function set(key, value) {
     sourceObject[key] = value
     return sourceObject[key]
-}
-
-
-// Test get function
-console.log(get("num")); // Should output 42
-console.log(get("bool")); // Should output true
-console.log(get("str")); // Should output 'some text'
-
-// Test set function
-console.log(set("num", 77)); // Should output 77
-console.log(set("bool", false)); // Should output false
-console.log(set("str", "new text")); // Should output 'new text'
-
-// Verify the changes
-console.log(get("num")); // Should output 77
-console.log(get("bool")); // Should output false
-console.log(get("str")); // Should output 'new text'
+};
