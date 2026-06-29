856284b1647fce1a394e3fb9e2986349b63fb9ed
osaddouq <saddouqoussama@gmail.com>
2026-06-29 17:10:57 +0100

    lastt-first-kiss

--- last-first-kiss.js
+++ last-first-kiss.js
@@ -10,14 +10,3 @@ function kiss(str){
 
 
 
-// // Test the first function
-console.log(first([1, 2, 3])); // Should output 1
-console.log(first("hello")); // Should output "h"
-
-// Test the last function
-console.log(last([1, 2, 3])); // Should output 3
-console.log(last("hello")); // Should output "o"
-
-// Test the kiss function
-console.log(kiss([1, 2, 3])); // Should output [3, 1]
-console.log(kiss("hello")); // Should output ["o", "h"]
