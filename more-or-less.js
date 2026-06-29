522dd4418c55363f59cdfac5602291e1e268324a
osaddouq <saddouqoussama@gmail.com>
2026-06-29 16:30:11 +0100

    more or less

---
+++ more-or-less.js
@@ -0,0 +1,14 @@
+function more (more){
+    return more +1
+}
+function less(less){
+    return less -1
+}
+function add(add1 , add2){
+    return add1 + add2 
+}
+function sub(sub1 , sub2){
+    return sub1 - sub2 
+}
+
+
