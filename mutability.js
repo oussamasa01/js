f35f1004726b95101e241a9eb0017520f91f8c8c
osaddouq <saddouqoussama@gmail.com>
2026-06-29 16:14:56 +0100

    mutability

---
+++ mutability.js
@@ -0,0 +1,31 @@
+const person = {
+    name: "Rick",
+    age: 77,
+    country: "US",
+};
+const clone1 = Object.assign({}, person)
+const clone2 = { ...person }
+const samePerson = person
+
+person.age ++
+person.country = 'FR'
+
+
+
+
+
+
+
+
+
+console.log(person.age === 78); // Should be true
+console.log(person.country === "FR"); // Should be true
+
+console.log(clone1.age === 77); // Should be true
+console.log(clone1.country === "US"); // Should be true
+
+console.log(clone2.age === 77); // Should be true
+console.log(clone2.country === "US"); // Should be true
+
+console.log(samePerson.age === 78); // Should be true
+console.log(samePerson.country === "FR"); // Should be true
