34b46084fee3b45aa855dbc5f6b277167edb46ee
osaddouq <saddouqoussama@gmail.com>
2026-06-30 16:13:13 +0100

    abs

---
+++ abs.js
@@ -0,0 +1,13 @@
+function isPositive(nm){
+    if(nm > 0 ){
+        return true
+    }else {
+        return false
+    }
+}
+function abs (nm){
+    if (nm < 0){
+        return -nm
+    }
+return nm
+}
