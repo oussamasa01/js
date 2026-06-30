dd52e16486c78df8d323669ad3d0987feb59e11f
osaddouq <saddouqoussama@gmail.com>
2026-06-30 14:53:16 +0100

    methode-man

--- method-man.js
+++ method-man.js
@@ -2,7 +2,7 @@ function words (str){
     return str.split(" ")
 }
 function sentence (str){
- return str.join()
+ return str.join(" ")
 }
 
 function yell(str){
@@ -15,3 +15,22 @@ function capitalize(str){
     return str[0].toUpperCase() + str.slice (1).toLowerCase()
 }
 
+// Test words function
+console.log(words("Hello there, how are you?"));
+// Should output: ["Hello", "there,", "how", "are", "you?"]
+
+// Test sentence function
+console.log(sentence(["This", "is", "a", "test"]));
+// Should output: "This is a test"
+
+// Test yell function
+console.log(yell("be quiet"));
+// Should output: "BE QUIET"
+
+// Test whisper function
+console.log(whisper("EXCUSE ME"));
+// Should output: "*excuse me*"
+
+// Test capitalize function
+console.log(capitalize("rUNNING"));
+// Should output: "Running"
