f2ffebcd20455625056af07a18d8a88c712b9fab
osaddouq <saddouqoussama@gmail.com>
2026-07-01 14:43:14 +0100

    elementry

--- elementary.js
+++ elementary.js
@@ -1,25 +1,61 @@
 function multiply(a, b) {
     let res = 0;
+    let sign = 1
+    if (a < 0 ){
+        sign =-sign
+        a = -a
+    }
+     if (b < 0 ){
+        sign =-sign
+        b = -b
+    }
+    
     for (let i = 0; i < b; i++) {
         res += a;
+    }
+    if (sign < 0 ){
+        return -res
     }
     return res;
 }
 function divide(a, b) {
     let c =0;
+    let sign = 1
+      if (a < 0 ){
+        sign =-sign
+        a = -a
+    }
+     if (b < 0 ){
+        sign =-sign
+        b = -b
+    }
+
     while (a >=b){
         a-=b;
         c++;
        
     }
+     if (sign < 0 ){
+        return -c
+    }
   return c ;
-
-
 }
 function modulo (a,b ){
-   
+    let sign = 1
+   if (a < 0 ){
+        sign =-1
+        a = -a
+    }
+     if (b < 0 ){
+        
+        b = -b
+    }
+
     while (a>=b){
         a-=b;
+    }
+     if (sign < 0 ){
+        return -a
     }
     return a;
 }
